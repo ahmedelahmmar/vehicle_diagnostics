5f7734ef18edc07a433815e7c2b9fa364c266f4e
ahmedelahmmar <ahmedelahmmar@gmail.com>
2024-12-21 22:30:56 +0200

    first app prototype

--- include/app.h
+++ include/app.h
@@ -11,6 +11,11 @@
 #include "adc.h"
 #include "ultrasonic.h"
 
+
+#define DTC_ACCIDENT_MIGHT_HAPPENED "P001 - ACCIDENT"
+#define DTC_ENGINE_HIGH_TEMPERATURE "P002 - HIGH TEMP"
+
 void handleMotors(void);
+void start_app(void);
 
 #endif /* __APP_H__*/
--- main.c
+++ main.c
@@ -1,59 +1,84 @@
 #include "./include/app.h"
-#define DTC_ACCIDENT_MIGHT_HAPPENED "P001 - ACCIDENT"
-#define DTC_ENGINE_HIGH_TEMPERATURE "P002 - HIGH TEMP"
 
-void Leds_Init(void)
-{
-    SET_BIT(SYSCTL_RCGCGPIO_REG, 5);
-    while(GET_BIT(SYSCTL_PRGPIO_REG, 5) == 0);
-
-    GPIO_PORTF_AMSEL_REG &= 0xF1;
-    GPIO_PORTF_PCTL_REG  &= 0xFFFF000F;
-    GPIO_PORTF_DIR_REG   |= 0x0E;
-    GPIO_PORTF_AFSEL_REG &= 0xF1;
-    GPIO_PORTF_DEN_REG   |= 0x0E;
-    GPIO_PORTF_DATA_REG  &= 0xF1;
-}
 
 int temp;
 int dist;
 
+volatile recieved_byte = 0xFF;
+
 int main(void)
 {
-    pb_init();
+    // pb_init();
 
-    adc0_init();
+    // adc0_init();
 
-    lcd_init();
-    delay_ms(200);
-    lcd_set_cursor(0, 0);
-    lcd_write_string("T:");
-    lcd_set_cursor(0, 5);
-    lcd_write_string("C");
-    lcd_set_cursor(0, 7);
-    lcd_write_string("D:");
-    lcd_set_cursor(0, 13);
-    lcd_write_string("cm");
-    lcd_set_cursor(1, 0);
-    lcd_write_string("L:Closed");
-    lcd_set_cursor(1, 8);
-    lcd_write_string("R:Closed");
+    // lcd_init();
+    // delay_ms(200);
+    // lcd_set_cursor(0, 0);
+    // lcd_write_string("T:");
+    // lcd_set_cursor(0, 5);
+    // lcd_write_string("C");
+    // lcd_set_cursor(0, 7);
+    // lcd_write_string("D:");
+    // lcd_set_cursor(0, 13);
+    // lcd_write_string("cm");
+    // lcd_set_cursor(1, 0);
+    // lcd_write_string("L:Closed");
+    // lcd_set_cursor(1, 8);
+    // lcd_write_string("R:Closed");
+    // Ultrasonic_Init();
 
-    Ultrasonic_Init();
+    // while(1)
+    // {
+    //     handleMotors();
+    //     temp = adc0_temp();
+    //     lcd_set_cursor(0,5);
+    //     lcd_write_int(temp);
 
-    while(1)
-    {
-        handleMotors();
-        temp = adc0_temp();
-        lcd_set_cursor(0,2);
-        lcd_write_int(temp);
-
-        dist = Ultrasonic_ReadDistance();
-        lcd_set_cursor(0, 9);
-        lcd_write_string("    ");
-        lcd_set_cursor(0, 9);
-        lcd_write_int(dist);
+    //     dist = Ultrasonic_ReadDistance();
+    //     lcd_set_cursor(0, 9);
+    //     lcd_write_string("    ");
+    //     lcd_set_cursor(0, 9);
+    //     lcd_write_int(dist);
     
-        delay_ms(1000);
+    //     delay_ms(1000);
+    // }
+
+    uart0_init();
+
+    uart0_sendString("Vehicle Fault Detection and Logging System (VFDLS)\n\n\r");
+    uart0_sendString("Please select one of the following options:\n\r");
+    uart0_sendString("1. Start Operation Cycle.\n\r");
+    uart0_sendString("2. Retrieve Logged Errors.\n\r");
+
+
+    while (1)
+    {
+        recieved_byte = uart0_getLastRecievedByte();
+
+        if (0xFF != recieved_byte)
+        {
+            if ('1' == recieved_byte)
+            {
+                uart0_sendString("\n\rInitializing Operation Cycle");
+
+                for (uint8_t i = 0; i < 10; i++)
+                {
+                    uart0_sendByte('.');
+                    delay_ms(250);
+                }
+
+                uart0_sendString("\n\rOperation Started Succesfully\n\r");
+                uart0_sendString("Press 3 to terminate the operation cycle");
+
+                // start_app();
+            }
+            else if ('2' == recieved_byte)
+            {
+                eeprom_retrieve_logged_errors();
+            }
+        }
     }
+
+
 }
